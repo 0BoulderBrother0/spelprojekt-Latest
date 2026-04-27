dd634517db6a3fd349632efc9aa41464fb67871d
Daniel Samuelsson <dsamuelsson08@icloud.com>
2026-04-27 14:31:44 +0200

    Slut lektion 27/04
    Lade till en player med player movement och platformar med platform colliders. Fixat halvt fungerande hopp för playern

---
+++ Assets/Scripts/GroundCheckScript.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class GroundCheckScript : MonoBehaviour
+{
+
+    public static bool isOnGround;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ground"))
+        {
+            isOnGround = false;
+        }
+    }
+}
